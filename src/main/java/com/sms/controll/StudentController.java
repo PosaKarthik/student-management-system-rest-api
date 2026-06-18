91916d7236dfef926cfc7331d153e062d9150ad8
Posa Karthik <posakarthik16@gmail.com>
2026-06-18 11:57:02 +0530

    Intgerated Swagger OpenAI documnetation

--- src/main/java/com/sms/controll/StudentController.java
+++ src/main/java/com/sms/controll/StudentController.java
@@ -3,6 +3,9 @@ package com.sms.controll;
 import com.sms.dto.StudentRequestDTO;
 import com.sms.dto.StudentResponseDTO;
 import com.sms.service.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/students")
+@Tag(name="Student API",description = "Student Management Operations")
 public class StudentController {
 
     @Autowired
@@ -18,6 +22,7 @@ public class StudentController {
 
 
     @PostMapping
+    @Operation(summary = "Add Student")
     public ResponseEntity<StudentResponseDTO> addStudent(@RequestBody StudentRequestDTO studentRequestDTO){
 
         StudentResponseDTO studentResponseDTO= studentService.addStudent(studentRequestDTO);
@@ -27,23 +32,27 @@ public class StudentController {
 
 
     @GetMapping
+    @Operation(summary = "Get All Students")
     public ResponseEntity<List<StudentResponseDTO>> getAllStudents(){
         return ResponseEntity.ok(studentService.getAllStudents());
     }
 
     @GetMapping("/{studentId}")
-    public ResponseEntity<StudentResponseDTO> getStudentById(@PathVariable Integer studentId){
+    @Operation(summary = "Get Student By Id")
+    public ResponseEntity<StudentResponseDTO> getStudentById(@Parameter(description = "Student Id") @PathVariable Integer studentId){
         return  ResponseEntity.ok(studentService.getById(studentId));
     }
 
     @PutMapping("/{studentId}")
-    public ResponseEntity<StudentResponseDTO> updateStudent(@PathVariable Integer studentId,
+    @Operation(summary = "Update Student")
+    public ResponseEntity<StudentResponseDTO> updateStudent(@Parameter(description = "Student Id") @PathVariable Integer studentId,
                                                             @RequestBody StudentRequestDTO studentRequestDTO){
         return ResponseEntity.ok(studentService.updateStudent(studentId,studentRequestDTO));
     }
 
     @DeleteMapping("/{studentId}")
-    public ResponseEntity<String> deleteStudent(@PathVariable Integer studentId){
+    @Operation(summary = "Delete Student")
+    public ResponseEntity<String> deleteStudent(@Parameter(description = "Student Id") @PathVariable Integer studentId){
         studentService.deleteStudent(studentId);
         return ResponseEntity.ok("Student deleted successfully");
     }
